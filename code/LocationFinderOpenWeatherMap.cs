d39f4166337d7ace9fd84ba21cbdcd058cc8dc4b
Clemens Hentschke <c.hentschke@dkfz-heidelberg.de>
2021-02-14 00:22:50 +0100

    Adapt location finder for new API

--- code/LocationFinderOpenWeatherMap.cs
+++ code/LocationFinderOpenWeatherMap.cs
@@ -1,34 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Xml;
+using System.Globalization;
 
 namespace TravelWeather
 {
     class LocationFinderOpenWeatherMap : LocationFinderBase
     {
         protected Dictionary<int, String> searchModeString;
+        protected String APIKey = "";
 
         public LocationFinderOpenWeatherMap(string name)
         {
             this.parseName(name);
             this.searchMode = 0;
-            searchModeString = new Dictionary<int,string>();
+            searchModeString = new Dictionary<int, string>();
             searchModeString.Add(0, "accurate");
             searchModeString.Add(1, "like");
         }
 
-        public override void find(){
-            String url = "";
+        public override void find()
+        {
+            //decimal delimiter is .
+            CultureInfo culture = new CultureInfo("en-US");
             String value = "";
             searchModeString.TryGetValue(searchMode, out value);
-            if (countryName == "")
+            String search = this.cityName;
+            if (String.IsNullOrEmpty(countryName) == false)
             {
-                url = "http://api.openweathermap.org/data/2.5/find?q=" + this.cityName + "&type=" + value +"&mode=xml";
+                search += "," + this.countryName;
             }
-            else
-            {
-                url = "http://api.openweathermap.org/data/2.5/find?q=" + this.cityName + "," + this.countryName + "&type=" + value + "&mode=xml";
-            }
+            String url = "https://api.openweathermap.org/data/2.5/find?q=" + search + "&type=" + value + "&mode=xml" + "&APPID=" + this.APIKey;
             XmlDocument doc = new XmlDocument();
             doc.Load(url);
             XmlNodeList listOfCities = doc.SelectNodes("/cities/list/item");
@@ -41,10 +43,10 @@ namespace TravelWeather
                 XmlNode country = city["country"];
                 XmlNode sun = city["sun"];
                 XmlAttributeCollection cityAttributes = city.Attributes;
-                Location loc = new Location(city.Attributes["name"].Value, country.InnerText, int.Parse(city.Attributes["id"].Value), "OpenWeatherMap", double.Parse(coord.Attributes["lon"].Value), double.Parse(coord.Attributes["lat"].Value));
+                Location loc = new Location(city.Attributes["name"].Value, country.InnerText, int.Parse(city.Attributes["id"].Value), "OpenWeatherMap", double.Parse(coord.Attributes["lon"].Value, culture.NumberFormat), double.Parse(coord.Attributes["lat"].Value, culture.NumberFormat));
+                loc.toString();
                 this.possibleLocations.Add(loc);
             }
-
         }
     }
 }
